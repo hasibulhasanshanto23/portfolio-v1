34d473dd2d41fbe17843932a3fbd5033cf69564f
hasibulhasanshanto23 <hasibulhasan23sept@gmail.com>
2025-01-16 17:33:31 +0600

    develop branch created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-[60px] underline ">Hasibul Hasan</h1>
       <h3 className="text-3xl underline">Software Engineer</h3>
+      <h6 className="text-3xl underline">Under development!</h6>
     </div>
   );
 }
